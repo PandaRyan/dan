eedf42ab014afdd477e09d8bded01690bdd4829e
Ryan Ng <ryanng.kawai08@gmail.com>
2026-04-18 15:12:30 +0800

    nodejs express init

--- umh_api/app.js
+++ umh_api/app.js
@@ -1,24 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
 
-var app = express();
+const express = require('express');
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-app.use(logger('dev'));
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL)
+    console.log("DB Connected successfully")
+  } catch (err) {
+    console.error("Database connection failed")
+    process.exit(1)
+  }
+}
+connectDB()
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -30,12 +37,10 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+    message:err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
 });
 
 module.exports = app;
