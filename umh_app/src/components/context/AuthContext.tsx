51b2a31940e0b7bf6327bba76b9e554f5c571cd6
jdyew37 <jdyewjh@gmail.com>
2026-04-22 02:29:23 +0800

    fix run lent code quality

--- umh_app/src/components/context/AuthContext.tsx
+++ umh_app/src/components/context/AuthContext.tsx
@@ -20,6 +20,7 @@ interface AuthProviderProps {
   children: ReactNode;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useAuth() {
   const context = useContext(AuthContext)
   if (context === undefined) 
