26b2395194015637275338c4b34dcf413f314416
jdyew37 <jdyewjh@gmail.com>
2026-04-22 21:38:52 +0800

    finish the prompt bar

---
+++ umh_app/src/components/PromptBar.tsx
@@ -0,0 +1,54 @@
+import { AppBar, Toolbar, Typography, TextField, Button, Box, IconButton, } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+
+export const PromptBar = () => {
+  return (
+    <Box sx={{
+      position: 'fixed',
+      bottom: '15%',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      width: '700px',
+    }}>
+      <AppBar
+        position="static"
+        sx={{
+          borderRadius: 5,
+          backgroundColor: '#BDA891',
+        }}
+      >
+        <Toolbar sx={{
+          alignItems: 'flex-end', 
+          pb: 2,                  
+          pt: 2,
+
+        }}
+        >
+          <TextField
+            multiline
+            size="small"
+            placeholder="Enter prompt..."
+            sx={{
+              backgroundColor: '#BDA891',
+              borderRadius: 1,
+              flexGrow: 1,
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: 'transparent',
+                },
+                '&:hover fieldset': {
+                  borderColor: '#6C513C',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: '#6C513C',
+                },
+              }
+            }}
+          />
+          <IconButton sx={{ ml: 1 }}><SendIcon /></IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
